cc1bccc6165b8daa096628b0f4e7e8cbe329c65f
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-08-29 13:56:43 -0300

    expanded share test

--- server.js
+++ server.js
@@ -24,9 +24,11 @@ app.get('/share/:id', async (req, res) => {
    const post = req.url.split('/')[2].split('?')[0]
    console.log(post)
    //res.sendFile(`public/${post}.html`, {root: __dirname})
+   const allPosts = await Share.find()
+   console.log(allPosts)
    const selectedPost = await Share.findOne({ postId: post }).exec()
    console.log(selectedPost)
-   res.json(selectedPost.content)
+   res.json(selectedPost)
 })
 
 app.use(express.json({limit: "500mb", extended: true}))
