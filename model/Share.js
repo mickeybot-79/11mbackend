f0b38c71e7499ae51a03bfc7870f1ced0244d539
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-08-29 14:00:35 -0300

    further expanded test

--- model/Share.js
+++ model/Share.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const shareSchema = new Schema({
-    postId: String,
-    content: String
+    content: String,
+    postId: String
 })
 
 module.exports = mongoose.model('Share', shareSchema)
