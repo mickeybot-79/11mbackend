ce1df5badbc8d7f0a7ae2be1f1589f4e1e9784e4
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-08-29 14:11:31 -0300

    added more test

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -28,7 +28,8 @@ const shareTest = async (req, res) => {
             postId: post,
             content: initialText
         })
-        res.status(201).json(result)
+        console.log(result)
+        //res.status(200).json(result)
     } catch (err) {
         console.log(err)
         res.status(500).json({ 'message': err.message })
