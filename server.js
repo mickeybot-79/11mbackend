600517ff9d444628126cb34d09e17750ad3d6ea2
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-08-30 13:37:36 -0300

    moved share controller to server

--- server.js
+++ server.js
@@ -9,12 +9,25 @@ const mongoose = require('mongoose')
 const connectDB = require('./config/dbConn')
 const PORT = process.env.PORT || 3500
 
+const Post = require('./model/Post')
+
 connectDB()
 
 //app.use(credentials)
 
 app.use(cors())
 //app.use(cors(corsOptions))
+
+
+app.get('/share/:id', async (req, res) => {
+   //console.log(req.url)
+   const post = req.url.split('/')[2].split('?')[0]
+   console.log(post)
+   const selectedPost = await Post.findOne({ searchField: post }).exec()
+   //console.log(selectedPost)
+   res.send(selectedPost.share)
+})
+
 
 app.use(express.json({limit: "500mb", extended: true}))
 app.use(express.urlencoded({limit: "500mb", extended: true, parameterLimit: 50000}))
