4846fbb23e376dae88b595c57388e44c512cf852
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-09-03 18:38:19 -0300

    added share origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,10 @@
 const allowedOrigins = [
     //'null',
-    'http://localhost:3000',
-    'https://oncemetros.onrender.com'
+    //'http://localhost:3000',
+    'https://oncemetros.onrender.com',
+    'https://facebook.com',
+    'https://x.com',
+    'https://web.whatsapp.com/'
  ]
 
  module.exports = allowedOrigins
