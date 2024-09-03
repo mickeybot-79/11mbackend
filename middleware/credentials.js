a8f764a0c3642f7636cf8f43c6e0f636e78bed47
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-09-03 17:11:03 -0300

    expanded test

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -2,6 +2,7 @@ const allowedOrigins = require('../config/allowedOrigins')
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin
+    console.log('origin', origin)
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true)
         res.header('Access-Control-Allow-Origin', true)
